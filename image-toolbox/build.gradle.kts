4c58bfbf00c8958077b844d2d77561f03ab3078f
Malik Mukhametzyanov <oolbp@bk.ru>
2024-03-21 07:50:46 +0300

    Update build.gradle.kts

--- image-toolbox/build.gradle.kts
+++ image-toolbox/build.gradle.kts
@@ -26,7 +26,7 @@ afterEvaluate {
             create<MavenPublication>("mavenJava") {
                 groupId = "com.github.t8rin"
                 artifactId = "imageToolboxLibs"
-                version = "1.2.4"
+                version = "1.2.5"
                 from(components["release"])
             }
         }
