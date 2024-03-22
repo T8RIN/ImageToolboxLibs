227241e18229b0d9d45ba8f188c52771d40d98a1
T8RIN <oolbp@bk.ru>
2024-03-22 09:18:46 +0300

    1.2.7

--- image-toolbox/build.gradle.kts
+++ image-toolbox/build.gradle.kts
@@ -26,7 +26,7 @@ afterEvaluate {
             create<MavenPublication>("mavenJava") {
                 groupId = "com.github.t8rin"
                 artifactId = "imageToolboxLibs"
-                version = "1.2.6"
+                version = "1.2.7"
                 from(components["release"])
             }
         }
