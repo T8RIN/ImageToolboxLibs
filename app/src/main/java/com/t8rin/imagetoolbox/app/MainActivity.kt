580c87caaf5a89a7e5a1ef97c1b53bee917f09e1
T8RIN <oolbp@bk.ru>
2024-06-06 13:27:58 +0300

    test

--- app/src/main/java/com/t8rin/imagetoolbox/app/MainActivity.kt
+++ app/src/main/java/com/t8rin/imagetoolbox/app/MainActivity.kt
@@ -1,42 +1,11 @@
 package com.t8rin.imagetoolbox.app
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Path
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.PickVisualMediaRequest
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import com.t8rin.imagetoolbox.app.ui.theme.ImageToolboxLibsTheme
-import com.t8rin.logger.makeLog
-import jp.co.cyberagent.android.gpuimage.GPUImageNativeLibrary
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,101 +14,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ImageToolboxLibsTheme {
                 Surface {
-                    var bitmap by remember {
-                        mutableStateOf<Bitmap?>(null)
-                    }
-                    var path by remember {
-                        mutableStateOf<Path?>(null)
-                    }
-                    val context = LocalContext.current
-                    LaunchedEffect(bitmap) {
-                        if (bitmap == null) {
-//                            bitmap = Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
-//                            Canvas(bitmap!!).apply {
-//                                drawColor(Color.Green.toArgb())
-//                                drawRect(
-//                                    200f, 200f, 400f, 400f,
-//                                    android.graphics.Paint().apply {
-//                                        style = android.graphics.Paint.Style.FILL
-//                                        color = Color.Red.toArgb()
-//                                    }
-//                                )
-//                            }
-                            val bmp =
-                                BitmapFactory.decodeResource(context.resources, R.drawable.test2)
-                            bitmap = bmp
-                            path = null
-                        }
-                    }
-                    val scope = rememberCoroutineScope()
-                    val imagePicker =
-                        rememberLauncherForActivityResult(contract = ActivityResultContracts.PickVisualMedia()) {
-                            scope.launch {
-                                bitmap = BitmapFactory.decodeStream(
-                                    it?.let { it1 ->
-                                        context.contentResolver.openInputStream(it1)
-                                    }
-                                )
-                            }
-                        }
-                    Column {
-                        bitmap?.asImageBitmap()?.let {
-                            Image(
-                                bitmap = it,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .weight(1f)
-                                    .padding(top = 48.dp)
-                                    .pointerInput(Unit) {
-                                        detectTapGestures {
-                                            val copied =
-                                                bitmap!!.copy(Bitmap.Config.ARGB_8888, true)
-                                            scope.launch(Dispatchers.IO) {
-                                                bitmap = GPUImageNativeLibrary
-                                                    .cropToContent(
-                                                        bitmap = copied,
-                                                        colorToIgnore = Color.Black.toArgb(),
-                                                        tolerance = 0f
-                                                    )
-                                                    .makeLog("COCK")
-                                            }
-                                        }
-                                    },
-                                contentDescription = null
-                            )
-                        } //{
-//                            bitmap?.asImageBitmap()?.let { drawImage(it) }
-//                            path?.asComposePath()?.let { drawPath(it, Color.Green) }
-//                            path?.asImageBitmap()?.let { drawImage(it) }
-//                            path?.asComposePath()?.let { drawPath(it, Color.Green) }
-//                            path.forEach {
-//                                drawContext.canvas.nativeCanvas.drawPoint(
-//                                    it.x.toFloat(),
-//                                    it.y.toFloat(),
-//                                    Paint().apply {
-//                                        color = Color.Green.toArgb()
-//                                    }
-//                                )
-//                            }
-//                        }
-
-                        Row {
-                            Button(onClick = {
-                                path = null
-                                bitmap = BitmapFactory.decodeResource(
-                                    context.resources,
-                                    R.drawable.test2
-                                )
-                            }) {
-                                Text("Reset")
-                            }
-                            Button(onClick = {
-                                imagePicker.launch(PickVisualMediaRequest())
-                            }) {
-                                Text("Pick")
-                            }
-                        }
-                    }
+                    RotationCropHypothesis()
                 }
             }
         }
---
+++ app/src/main/java/com/t8rin/imagetoolbox/app/RotationCropHypothesis.kt
@@ -0,0 +1,100 @@
+package com.t8rin.imagetoolbox.app
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asAndroidBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.unit.dp
+import com.smarttoolfactory.cropper.ImageCropper
+import com.smarttoolfactory.cropper.model.OutlineType
+import com.smarttoolfactory.cropper.model.RectCropShape
+import com.smarttoolfactory.cropper.settings.CropDefaults
+import com.smarttoolfactory.cropper.settings.CropOutlineProperty
+import kotlinx.coroutines.launch
+
+@Composable
+fun MainActivity.RotationCropHypothesis() {
+    var bitmap by remember {
+        mutableStateOf<Bitmap?>(null)
+    }
+    val scope = rememberCoroutineScope()
+    val imagePicker =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.PickVisualMedia()) {
+            scope.launch {
+                bitmap = BitmapFactory.decodeStream(
+                    it?.let { it1 ->
+                        contentResolver.openInputStream(it1)
+                    }
+                )
+            }
+        }
+
+    val pickImage: () -> Unit = {
+        imagePicker.launch(PickVisualMediaRequest())
+    }
+
+    var crop by remember {
+        mutableStateOf(false)
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp)
+    ) {
+        val img = remember(bitmap) { bitmap?.asImageBitmap() }
+        img?.let { imageBitmap ->
+            ImageCropper(
+                imageBitmap = imageBitmap,
+                cropProperties = CropDefaults.properties(
+                    cropOutlineProperty = CropOutlineProperty(
+                        OutlineType.Rect,
+                        RectCropShape(
+                            id = 0,
+                            title = OutlineType.Rect.name
+                        )
+                    ),
+                    fling = true,
+                    rotatable = true
+                ),
+                crop = crop,
+                onCropStart = { },
+                onZoomChange = {},
+                onCropSuccess = {
+                    bitmap = it.asAndroidBitmap()
+                    crop = false
+                },
+                modifier = Modifier.weight(1f)
+            )
+        }
+
+        Row {
+            Button(onClick = pickImage) {
+                Text("Pick")
+            }
+            Button(
+                onClick = {
+                    crop = true
+                }
+            ) {
+                Text("Crop")
+            }
+        }
+    }
+}
