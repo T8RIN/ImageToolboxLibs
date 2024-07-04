a92d4e2dc7155b53b60f5cba11f8762961f203f4
T8RIN <oolbp@bk.ru>
2024-07-05 01:40:42 +0300

    some fixes to tiff

--- libs/tiffdecoder/build.gradle.kts
+++ libs/tiffdecoder/build.gradle.kts
@@ -11,8 +11,20 @@ android {
     }
     defaultConfig {
         ndk.abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
+
+        sourceSets.named("main") {
+            jniLibs {
+                srcDir("src/main/jniLibs")
+            }
+        }
     }
     ndkVersion = "21.3.6528147"
+
+    sourceSets.named("main") {
+        jniLibs {
+            srcDir("src/main/jniLibs")
+        }
+    }
 }
 
 dependencies {
