35a6ce62cf3fcc5f3d50dce751ee450096dd5fc7
T8RIN <oolbp@bk.ru>
2024-08-29 04:48:44 +0300

    improve decoders 2.3.1

--- libs/avif/src/main/java/com/t8rin/avif/AnimatedAvifDecoder.kt
+++ libs/avif/src/main/java/com/t8rin/avif/AnimatedAvifDecoder.kt
@@ -5,53 +5,37 @@ import com.github.penfeizhou.animation.avif.decode.AVIFDecoder
 import com.github.penfeizhou.animation.decode.FrameSeqDecoder
 import com.github.penfeizhou.animation.io.FileReader
 import com.github.penfeizhou.animation.io.Reader
-import com.github.penfeizhou.animation.io.StreamReader
 import java.io.Closeable
 import java.io.File
-import java.io.InputStream
 import java.nio.ByteBuffer
 
 class AnimatedAvifDecoder private constructor(
-    private val decoder: AVIFDecoder
+    sourceFile: File,
+    private val onStart: (AnimatedAvifDecoder) -> Unit
 ) : AutoCloseable, Closeable {
+
+    private val decoder: AVIFDecoder = createDecoder(
+        FileReader(sourceFile)
+    ).apply { start() }
 
     val frameCount: Int
         get() = decoder.frameCount
 
     fun getFrame(index: Int): Bitmap = decoder.getFrameBitmap(index)
 
-    class Builder(
-        private val decoder: AVIFDecoder
-    ) {
-        constructor(sourceInputStream: InputStream) : this(
-            createDecoder(
-                StreamReader(
-                    sourceInputStream
-                )
-            )
-        )
-
-        constructor(sourceFile: File) : this(
-            createDecoder(
-                FileReader(sourceFile)
-            )
-        )
-
-        fun build(): AnimatedAvifDecoder = AnimatedAvifDecoder(decoder).also {
-            decoder.start()
-        }
-    }
-
-    companion object {
-        private fun createDecoder(reader: Reader): AVIFDecoder = AVIFDecoder(
-            { reader },
-            object : FrameSeqDecoder.RenderListener {
-                override fun onStart() = Unit
-                override fun onRender(byteBuffer: ByteBuffer?) = Unit
-                override fun onEnd() = Unit
+    private fun createDecoder(
+        reader: Reader
+    ): AVIFDecoder = AVIFDecoder(
+        { reader },
+        object : FrameSeqDecoder.RenderListener {
+            override fun onStart() {
+                this@AnimatedAvifDecoder.use(onStart)
             }
-        )
-    }
+
+            override fun onRender(byteBuffer: ByteBuffer?) = Unit
+            override fun onEnd() = Unit
+        }
+    )
 
     override fun close() = decoder.stop()
 
--- libs/awebp/src/main/java/com/t8rin/awebp/decoder/AnimatedWebpDecoder.kt
+++ libs/awebp/src/main/java/com/t8rin/awebp/decoder/AnimatedWebpDecoder.kt
@@ -4,55 +4,39 @@ import android.graphics.Bitmap
 import com.github.penfeizhou.animation.decode.FrameSeqDecoder
 import com.github.penfeizhou.animation.io.FileReader
 import com.github.penfeizhou.animation.io.Reader
-import com.github.penfeizhou.animation.io.StreamReader
 import com.github.penfeizhou.animation.webp.decode.WebPDecoder
 import java.io.Closeable
 import java.io.File
-import java.io.InputStream
 import java.nio.ByteBuffer
 
 class AnimatedWebpDecoder private constructor(
-    private val decoder: WebPDecoder
+    sourceFile: File,
+    private val onStart: (AnimatedWebpDecoder) -> Unit
 ) : AutoCloseable, Closeable {
+
+    private val decoder: WebPDecoder = createDecoder(
+        FileReader(sourceFile)
+    ).apply { start() }
 
     val frameCount: Int
         get() = decoder.frameCount
 
     fun getFrame(index: Int): Bitmap = decoder.getFrameBitmap(index)
 
-    override fun close() = decoder.stop()
-
-    class Builder(
-        private val decoder: WebPDecoder
-    ) {
-        constructor(sourceInputStream: InputStream) : this(
-            createDecoder(
-                StreamReader(
-                    sourceInputStream
-                )
-            )
-        )
-
-        constructor(sourceFile: File) : this(
-            createDecoder(
-                FileReader(sourceFile)
-            )
-        )
-
-        fun build(): AnimatedWebpDecoder = AnimatedWebpDecoder(decoder).also {
-            decoder.start()
-        }
-    }
-
-    companion object {
-        private fun createDecoder(reader: Reader): WebPDecoder = WebPDecoder(
-            { reader },
-            object : FrameSeqDecoder.RenderListener {
-                override fun onStart() = Unit
-                override fun onRender(byteBuffer: ByteBuffer?) = Unit
-                override fun onEnd() = Unit
+    private fun createDecoder(
+        reader: Reader
+    ): WebPDecoder = WebPDecoder(
+        { reader },
+        object : FrameSeqDecoder.RenderListener {
+            override fun onStart() {
+                this@AnimatedWebpDecoder.use(onStart)
             }
-        )
-    }
+
+            override fun onRender(byteBuffer: ByteBuffer?) = Unit
+            override fun onEnd() = Unit
+        }
+    )
+
+    override fun close() = decoder.stop()
 
 }
