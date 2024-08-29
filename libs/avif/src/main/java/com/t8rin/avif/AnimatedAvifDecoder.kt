c09b1fcb840cdfbc3c5badfa6b1e91a439df4785
T8RIN <oolbp@bk.ru>
2024-08-29 04:15:46 +0300

    improve decoders 2.3.0

--- libs/avif/src/main/java/com/t8rin/avif/AnimatedAvifDecoder.kt
+++ libs/avif/src/main/java/com/t8rin/avif/AnimatedAvifDecoder.kt
@@ -6,23 +6,41 @@ import com.github.penfeizhou.animation.decode.FrameSeqDecoder
 import com.github.penfeizhou.animation.io.FileReader
 import com.github.penfeizhou.animation.io.Reader
 import com.github.penfeizhou.animation.io.StreamReader
+import java.io.Closeable
 import java.io.File
 import java.io.InputStream
 import java.nio.ByteBuffer
 
 class AnimatedAvifDecoder private constructor(
     private val decoder: AVIFDecoder
-) {
-
-    constructor(sourceInputStream: InputStream) : this(createDecoder(StreamReader(sourceInputStream)))
-
-    constructor(sourceFile: File) : this(createDecoder(FileReader(sourceFile)))
-
+) : AutoCloseable, Closeable {
 
     val frameCount: Int
         get() = decoder.frameCount
 
     fun getFrame(index: Int): Bitmap = decoder.getFrameBitmap(index)
+
+    class Builder(
+        private val decoder: AVIFDecoder
+    ) {
+        constructor(sourceInputStream: InputStream) : this(
+            createDecoder(
+                StreamReader(
+                    sourceInputStream
+                )
+            )
+        )
+
+        constructor(sourceFile: File) : this(
+            createDecoder(
+                FileReader(sourceFile)
+            )
+        )
+
+        fun build(): AnimatedAvifDecoder = AnimatedAvifDecoder(decoder).also {
+            decoder.start()
+        }
+    }
 
     companion object {
         private fun createDecoder(reader: Reader): AVIFDecoder = AVIFDecoder(
@@ -34,5 +52,7 @@ class AnimatedAvifDecoder private constructor(
             }
         )
     }
+
+    override fun close() = decoder.stop()
 
 }
--- libs/awebp/src/main/java/com/t8rin/awebp/decoder/AnimatedWebpDecoder.kt
+++ libs/awebp/src/main/java/com/t8rin/awebp/decoder/AnimatedWebpDecoder.kt
@@ -6,23 +6,43 @@ import com.github.penfeizhou.animation.io.FileReader
 import com.github.penfeizhou.animation.io.Reader
 import com.github.penfeizhou.animation.io.StreamReader
 import com.github.penfeizhou.animation.webp.decode.WebPDecoder
+import java.io.Closeable
 import java.io.File
 import java.io.InputStream
 import java.nio.ByteBuffer
 
 class AnimatedWebpDecoder private constructor(
     private val decoder: WebPDecoder
-) {
-
-    constructor(sourceInputStream: InputStream) : this(createDecoder(StreamReader(sourceInputStream)))
-
-    constructor(sourceFile: File) : this(createDecoder(FileReader(sourceFile)))
-
+) : AutoCloseable, Closeable {
 
     val frameCount: Int
         get() = decoder.frameCount
 
     fun getFrame(index: Int): Bitmap = decoder.getFrameBitmap(index)
+
+    override fun close() = decoder.stop()
+
+    class Builder(
+        private val decoder: WebPDecoder
+    ) {
+        constructor(sourceInputStream: InputStream) : this(
+            createDecoder(
+                StreamReader(
+                    sourceInputStream
+                )
+            )
+        )
+
+        constructor(sourceFile: File) : this(
+            createDecoder(
+                FileReader(sourceFile)
+            )
+        )
+
+        fun build(): AnimatedWebpDecoder = AnimatedWebpDecoder(decoder).also {
+            decoder.start()
+        }
+    }
 
     companion object {
         private fun createDecoder(reader: Reader): WebPDecoder = WebPDecoder(
