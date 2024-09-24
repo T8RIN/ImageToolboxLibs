484e2bfd1a8edc4ba8491118fba228a7c8d3c320
T8RIN <oolbp@bk.ru>
2024-09-25 01:06:09 +0300

    2.8.3

--- libs/opencv-tools/build.gradle.kts
+++ libs/opencv-tools/build.gradle.kts
@@ -1,10 +1,12 @@
 plugins {
     alias(libs.plugins.image.toolbox.library)
     alias(libs.plugins.image.toolbox.maven)
+    alias(libs.plugins.image.toolbox.compose)
 }
 
 android.namespace = "com.t8rin.opencv_tools"
 
 dependencies {
     implementation(libs.opencv)
+    implementation(libs.documentscanner)
 }
---
+++ libs/opencv-tools/src/main/java/com/t8rin/opencv_tools/free_corners_crop/FreeCornersCropper.kt
@@ -0,0 +1,15 @@
+package com.t8rin.opencv_tools.free_corners_crop
+
+//import android.graphics.Bitmap
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.viewinterop.AndroidView
+//import com.websitebeaver.documentscanner.ui.ImageCropView
+//import com.websitebeaver.documentscanner.utils.ImageUtil
+//
+//@Composable
+//fun FreeCornersCropper(
+//    bitmap: Bitmap
+//) {
+//    ImageUtil().crop()
+//
+//}
