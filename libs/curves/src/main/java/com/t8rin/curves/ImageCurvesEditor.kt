92037b8f9af2980c67262663c51c89c2e4eae72a
T8RIN <oolbp@bk.ru>
2024-09-27 18:37:42 +0300

    curvessss

--- libs/curves/src/main/java/com/t8rin/curves/ImageCurvesEditor.kt
+++ libs/curves/src/main/java/com/t8rin/curves/ImageCurvesEditor.kt
@@ -14,8 +14,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.t8rin.curves.PhotoFilterCurvesControl.CurvesToolValue
+import com.t8rin.curves.PhotoFilterCurvesControl.CurvesValue
 import jp.co.cyberagent.android.gpuimage.GLTextureView
 import jp.co.cyberagent.android.gpuimage.GPUImage
+import jp.co.cyberagent.android.gpuimage.filter.GPUImageFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageToneCurveFilter
 
 internal object CurvesCache {
@@ -35,9 +37,7 @@ fun ImageCurvesEditor(
         mutableStateOf(
             GPUImage(context).apply {
                 setImage(bitmap)
-                setFilter(
-                    GPUImageToneCurveFilter()
-                )
+                setFilter(CurvesCache.buildFilter())
             }
         )
     }
@@ -56,56 +56,7 @@ fun ImageCurvesEditor(
             PhotoFilterCurvesControl(it, CurvesCache.curvesToolValue).apply {
                 setActualArea(0f, 0f, bitmap.width.toFloat(), bitmap.height.toFloat())
                 setDelegate {
-                    CurvesCache.curvesToolValue.fillBuffer()
-                    gpuImage.setFilter(
-                        GPUImageToneCurveFilter().apply {
-                            setRgbCompositeControlPoints(
-                                CurvesCache.curvesToolValue.luminanceCurve.let { curve ->
-                                    listOf(
-                                        PointF(0.0f, curve.blacksLevel / 100f),
-                                        PointF(0.25f, curve.shadowsLevel / 100f),
-                                        PointF(0.5f, curve.midtonesLevel / 100f),
-                                        PointF(0.75f, curve.highlightsLevel / 100f),
-                                        PointF(1.0f, curve.whitesLevel / 100f),
-                                    ).toTypedArray()
-                                }
-                            )
-                            setRedControlPoints(
-                                CurvesCache.curvesToolValue.redCurve.let { curve ->
-                                    listOf(
-                                        PointF(0.0f, curve.blacksLevel / 100f),
-                                        PointF(0.25f, curve.shadowsLevel / 100f),
-                                        PointF(0.5f, curve.midtonesLevel / 100f),
-                                        PointF(0.75f, curve.highlightsLevel / 100f),
-                                        PointF(1.0f, curve.whitesLevel / 100f),
-                                    ).toTypedArray()
-                                }
-                            )
-                            setGreenControlPoints(
-                                CurvesCache.curvesToolValue.greenCurve.let { curve ->
-                                    listOf(
-                                        PointF(0.0f, curve.blacksLevel / 100f),
-                                        PointF(0.25f, curve.shadowsLevel / 100f),
-                                        PointF(0.5f, curve.midtonesLevel / 100f),
-                                        PointF(0.75f, curve.highlightsLevel / 100f),
-                                        PointF(1.0f, curve.whitesLevel / 100f),
-                                    ).toTypedArray()
-                                }
-                            )
-                            setBlueControlPoints(
-                                CurvesCache.curvesToolValue.blueCurve.let { curve ->
-                                    listOf(
-                                        PointF(0.0f, curve.blacksLevel / 100f),
-                                        PointF(0.25f, curve.shadowsLevel / 100f),
-                                        PointF(0.5f, curve.midtonesLevel / 100f),
-                                        PointF(0.75f, curve.highlightsLevel / 100f),
-                                        PointF(1.0f, curve.whitesLevel / 100f),
-                                    ).toTypedArray()
-                                }
-                            )
-                            updateToneCurveTexture()
-                        }
-                    )
+                    gpuImage.setFilter(CurvesCache.buildFilter())
                 }
             }
         }
@@ -117,3 +68,27 @@ fun ImageCurvesEditor(
         }
     ) { }
 }
+
+private fun CurvesCache.buildFilter(): GPUImageFilter = GPUImageToneCurveFilter().apply {
+    setRgbCompositeControlPoints(
+        curvesToolValue.luminanceCurve.toPoints()
+    )
+    setRedControlPoints(
+        curvesToolValue.redCurve.toPoints()
+    )
+    setGreenControlPoints(
+        curvesToolValue.greenCurve.toPoints()
+    )
+    setBlueControlPoints(
+        curvesToolValue.blueCurve.toPoints()
+    )
+    updateToneCurveTexture()
+}
+
+private fun CurvesValue.toPoints(): Array<PointF> = listOf(
+    PointF(0.0f, blacksLevel / 100f),
+    PointF(0.25f, shadowsLevel / 100f),
+    PointF(0.5f, midtonesLevel / 100f),
+    PointF(0.75f, highlightsLevel / 100f),
+    PointF(1.0f, whitesLevel / 100f),
+).toTypedArray()
