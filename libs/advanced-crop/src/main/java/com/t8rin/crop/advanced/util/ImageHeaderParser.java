1978d1a8612b5da66375645360e23096bfe8b1b2
Malik Mukhametzyanov <oolbp@bk.ru>
2026-04-29 01:10:30 +0300

    fix cropper

--- libs/advanced-crop/src/main/java/com/t8rin/crop/advanced/util/ImageHeaderParser.java
+++ libs/advanced-crop/src/main/java/com/t8rin/crop/advanced/util/ImageHeaderParser.java
@@ -365,55 +365,62 @@ public class ImageHeaderParser {
         }
     }
 
-    private record StreamReader(InputStream is) implements Reader {
+    @SuppressWarnings("ClassCanBeRecord")
+    private static class StreamReader implements Reader {
+        private final InputStream is;
+
+        StreamReader(InputStream is) {
+            this.is = is;
+        }
+
         // Motorola / big endian byte order.
 
         @Override
-            public int getUInt16() throws IOException {
-                return (is.read() << 8 & 0xFF00) | (is.read() & 0xFF);
+        public int getUInt16() throws IOException {
+            return (is.read() << 8 & 0xFF00) | (is.read() & 0xFF);
+        }
+
+        @Override
+        public short getUInt8() throws IOException {
+            return (short) (is.read() & 0xFF);
+        }
+
+        @Override
+        public long skip(long total) throws IOException {
+            if (total < 0) {
+                return 0;
             }
 
-            @Override
-            public short getUInt8() throws IOException {
-                return (short) (is.read() & 0xFF);
-            }
-
-            @Override
-            public long skip(long total) throws IOException {
-                if (total < 0) {
-                    return 0;
-                }
-
-                long toSkip = total;
-                while (toSkip > 0) {
-                    long skipped = is.skip(toSkip);
-                    if (skipped > 0) {
-                        toSkip -= skipped;
+            long toSkip = total;
+            while (toSkip > 0) {
+                long skipped = is.skip(toSkip);
+                if (skipped > 0) {
+                    toSkip -= skipped;
+                } else {
+                    // Skip has no specific contract as to what happens when you reach the end of
+                    // the stream. To differentiate between temporarily not having more data and
+                    // having finished the stream, we read a single byte when we fail to skip any
+                    // amount of data.
+                    int testEofByte = is.read();
+                    if (testEofByte == -1) {
+                        break;
                     } else {
-                        // Skip has no specific contract as to what happens when you reach the end of
-                        // the stream. To differentiate between temporarily not having more data and
-                        // having finished the stream, we read a single byte when we fail to skip any
-                        // amount of data.
-                        int testEofByte = is.read();
-                        if (testEofByte == -1) {
-                            break;
-                        } else {
-                            toSkip--;
-                        }
+                        toSkip--;
                     }
                 }
-                return total - toSkip;
             }
-
-            @Override
-            public int read(byte[] buffer, int byteCount) throws IOException {
-                int toRead = byteCount;
-                int read;
-                while (toRead > 0 && ((read = is.read(buffer, byteCount - toRead, toRead)) != -1)) {
-                    toRead -= read;
-                }
-                return byteCount - toRead;
-            }
+            return total - toSkip;
         }
+
+        @Override
+        public int read(byte[] buffer, int byteCount) throws IOException {
+            int toRead = byteCount;
+            int read;
+            while (toRead > 0 && ((read = is.read(buffer, byteCount - toRead, toRead)) != -1)) {
+                toRead -= read;
+            }
+            return byteCount - toRead;
+        }
+    }
 
 }
